4fa4da802f65c864f56c3c5a866fa07f341f7b4b
eduardodgdebem <bemeduardode@gmail.com>
2024-12-03 18:48:14 -0300

    Refactoring

--- src/helper.zig
+++ src/helper.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const fs = std.fs;
+const io = std.io;
 
 pub fn bubbleSort(comptime T: type, arr: *std.ArrayList(T)) !void {
     var i: usize = 0;
@@ -14,4 +16,24 @@ pub fn bubbleSort(comptime T: type, arr: *std.ArrayList(T)) !void {
         }
         j = 0;
     }
+}
+
+pub fn readFile(allocator: std.mem.Allocator, file_name: []const u8) !std.ArrayList([]const u8) {
+    var f = try fs.cwd().openFile(file_name, .{});
+    defer f.close();
+
+    var buffered_reader = io.bufferedReader(f.reader());
+    var reader = buffered_reader.reader();
+    var lines = std.ArrayList([]const u8).init(allocator);
+    errdefer lines.deinit();
+
+    var buf: [std.mem.page_size]u8 = undefined;
+
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        const trimmed_line = std.mem.trim(u8, line, &std.ascii.whitespace);
+
+        try lines.append(try allocator.dupe(u8, trimmed_line));
+    }
+
+    return lines;
 }
--- src/part1.zig
+++ src/part1.zig
@@ -1,13 +1,11 @@
 const std = @import("std");
 const helper = @import("helper.zig");
-const fs = std.fs;
-const io = std.io;
 
 pub fn resolve() !u32 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
-    var lines = try readFile(allocator, "input_1.txt");
+    var lines = try helper.readFile(allocator, "input_1.txt");
 
     defer {
         for (lines.items) |line| {
@@ -26,34 +24,12 @@ pub fn resolve() !u32 {
     return calcDistance(&left, &right);
 }
 
-fn readFile(allocator: std.mem.Allocator, file_name: []const u8) !std.ArrayList([]const u8) {
-    var f = try fs.cwd().openFile(file_name, .{});
-    defer f.close();
-
-    var buffered_reader = io.bufferedReader(f.reader());
-    var reader = buffered_reader.reader();
-    var lines = std.ArrayList([]const u8).init(allocator);
-    errdefer lines.deinit();
-
-    var buf: [std.mem.page_size]u8 = undefined;
-
-    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        const trimmed_line = std.mem.trim(u8, line, &std.ascii.whitespace);
-
-        try lines.append(try allocator.dupe(u8, trimmed_line));
-    }
-
-    return lines;
-}
-
 fn splitLines(lines: *std.ArrayList([]const u8), left: *std.ArrayList(i32), right: *std.ArrayList(i32)) !void {
     for (lines.items) |line| {
         var iter = std.mem.tokenize(u8, line, "   ");
         const left_val = try std.fmt.parseInt(i32, iter.peek().?, 10);
         try left.append(left_val);
-
         _ = iter.next();
-
         const right_val = try std.fmt.parseInt(i32, iter.peek().?, 10);
         try right.append(right_val);
     }
